7040f5179f8c9db5f860a72459c8599f08bf4a0f
Kostas Stroggylos <stroggylos@gmail.com>
2012-08-25 23:45:51 +0300

    Added initial version of FRAMA-C metric plugin

---
+++ metrics/framac/src/main/java/eu/sqooss/metrics/framac/FramaCMetrics.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2008 - Organization for Free and Open Source Software,  
+ *                  Athens, Greece.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+** That copyright notice makes sense for code residing in the 
+** main SQO-OSS repository. For the Skeleton plug-in only, the Copyright
+** notice may be removed and replaced by a statement of your own
+** with (compatible) license terms as you see fit; the Skeleton
+** plug-in itself is insufficiently a creative work to be protected
+** by Copyright.
+*/
+
+/* This is the package for this particular plug-in. Third-party
+** applications will want a different package name, but it is
+** *ESSENTIAL* that the package name contain the string '.metrics.'
+** because this is how Alitheia Core discovers the metric plug-ins. 
+*/
+package eu.sqooss.metrics.framac;
+
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+
+/* These are imports of standard Alitheia core services and types.
+** You are going to need these anyway; some others that you might
+** need are the FDS and other Metric interfaces, as well as more
+** DAO types from the database service.
+*/
+import eu.sqooss.service.abstractmetric.AbstractMetric;
+import eu.sqooss.service.abstractmetric.MetricDecl;
+import eu.sqooss.service.abstractmetric.MetricDeclarations;
+import eu.sqooss.service.abstractmetric.Result;
+import eu.sqooss.service.db.Metric;
+import eu.sqooss.service.db.ProjectFile;
+
+/**
+ * Implementation of the FRAMA-C driver plug-in. It should be activated in 
+ * File, Directory or Project scope.
+ *  
+ */ 
+@MetricDeclarations(metrics= {
+	@MetricDecl(mnemonic="FramaC.DoubleFree", activators={ProjectFile.class}, descr="FramaC: Double Free Vulnerability"),
+	@MetricDecl(mnemonic="FramaC.FormatString", activators={ProjectFile.class}, descr="FramaC: Format String Vulnerability"),
+	@MetricDecl(mnemonic="FramaC.SQLInjection", activators={ProjectFile.class}, descr="FramaC: SQL Injection Vulnerability"),
+	@MetricDecl(mnemonic="FramaC.KernelTrust", activators={ProjectFile.class}, descr="FramaC: User Kernel Trust Error Vulnerability"),
+	@MetricDecl(mnemonic="FramaC.XSS", activators={ProjectFile.class}, descr="FramaC: XSS Vulnerability")
+})
+public class FramaCMetrics extends AbstractMetric {
+    
+    public FramaCMetrics(BundleContext bc) {
+        super(bc);        
+    }
+
+    public List<Result> getResult(ProjectFile a, Metric m) {
+        // Return a list of ResultEntries by querying the DB for the 
+        // measurements implement by the supported metric and calculated 
+        // for the specific project file.
+        return null;
+    }
+    
+    public void run(ProjectFile a) {
+        // 1. Get stuff related to the provided project file
+        // 2. Calculate one or more numbers
+        // 3. Store a result to the database
+    }
+    
+}
+
+// vi: ai nosi sw=4 ts=4 expandtab
