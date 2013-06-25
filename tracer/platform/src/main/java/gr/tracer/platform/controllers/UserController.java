85e498d3d077011c9d13ad02da4d0ae8a99f46fe
rafailidis <rafailidis@rafailidis-laptop.(none)>
2013-06-25 17:37:20 +0300

    Added annotations for User controller

--- tracer/platform/src/main/java/gr/tracer/platform/controllers/UserController.java
+++ tracer/platform/src/main/java/gr/tracer/platform/controllers/UserController.java
@@ -1,10 +1,15 @@
 package gr.tracer.platform.controllers;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 import eu.sqooss.service.db.User;
 import gr.tracer.platform.TracerPlatform;
 import gr.tracer.platform.components.UserComponent;
-import gr.tracer.platform.components.impl.UserComponentImpl;
 
+@Path("/controllers/user")
 public class UserController {
 	
 	UserComponent ucl = null;
@@ -20,7 +25,10 @@ public class UserController {
 	/**
      * @see gr.tracer.platform.components.UserComponent#createNewUser(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
      */
-	public User createTracerUser(String userName, String userPassword, String userType, String userEmail) {
+	@GET
+    @Produces({"application/xml", "application/json"})
+	@Path("/createUser/{userName}-{userPassword}-{userType}-{userEmail}")
+	public User createTracerUser(@PathParam("userName") String userName, @PathParam("userPassword") String userPassword, @PathParam("userType") String userType, @PathParam("userEmail") String userEmail) {
 		
 		return ucl.createNewUser(userName, userPassword, userType, userEmail);
 	}
@@ -28,8 +36,21 @@ public class UserController {
 	/**
      * @see gr.tracer.platform.components.UserComponent#userLoginAttempt(java.lang.String, java.lang.String)
      */
-	public User loginAttempt(String userName, String userPassword) {
+	@GET
+    @Produces({"application/xml", "application/json"})
+	@Path("/loginAttempt/{userName}-{userPassword}")
+	public User loginAttempt(@PathParam("userName") String userName, @PathParam("userPassword") String userPassword) {
 		
 		return ucl.userLoginAttempt(userName, userPassword);
+	}
+	
+	/**
+     * @see gr.tracer.platform.components.UserComponent#userExists(java.lang.String, java.lang.String)
+     */
+	@GET
+    @Produces({"application/xml", "application/json"})
+	@Path("/userExists/{userName}-{userPassword}")
+	public boolean userExists(@PathParam("userName") String userName, @PathParam("userPassword") String userPassword)  {
+		return ucl.userExists(userName, userPassword);
 	}
 }
