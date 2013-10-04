d9b3d69caa3d47b04a53c27ece9d93544644e86c
tzermias <zacharias.tzermias@gmail.com>
2013-10-04 10:59:11 +0300

    Enforce restrict access on some pages from routeProvider.
    Add access property to each route, specifying who can access this page
    (programmer, administrator). When someone with a role inferior from the one
    speicified on route, tries to access the page, it will be redirected to main
    page.
    
    For example when a user tries to access "/new-user" page (that access is
    restricted only to administrator) he will be redirected to /

--- tracer/webui/src/main/webapp/js/app.js
+++ tracer/webui/src/main/webapp/js/app.js
@@ -1,53 +1,67 @@
 
 angular.module('tracer', ['tracer.services', 'tracer.directives', 'tracer.controllers','ngCookies'])
 	.config(['$routeProvider', function($routeProvider) {
-	
+	//Redefine roles here...
+	var roles = {
+		'user' : 1, 		// 001
+		'programmer' : 2, 	// 010
+		'admin' : 4			// 100
+	};
 
 	$routeProvider.
 		when('/', {
 			templateUrl: 'includes/welcome.html', 
 			controller: 'WelcomeCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user,
 		}).
 		when('/login', {
 			templateUrl: "includes/login.html", 
 			controller: 'LoginController', 
-			requireLogin: false
+			requireLogin: false,
+			access: 0
 		}).
 		when('/logout', {
 			template: " ", 
 			controller: 'LogoutController', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		when('/new-user', {
 			templateUrl: 'includes/create-user.html', 
 			controller: 'NewUserCtrl', 
 			requireLogin: true,
+			access : roles.admin
 		}).
 		when('/createSecurityProfile', {
 			templateUrl: 'includes/createSecurityProfile.html', 
 			controller: 'CreateSecurityProfileCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		when('/addToolToProfile', {
 			templateUrl: 'includes/addToolToProfile.html', 
 			controller: 'AddToolToProfileCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		when('/addProjectToList', {
 			templateUrl: 'includes/addProjectToList.html', 
 			controller: 'AddProjectToListCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		when('/detectVulnerabilityToList', {
 			templateUrl: 'includes/detectVulnerabilityToList.html', 
 			controller: 'DetectVulnerabilityToListCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		when('/createOperationalModule', {
 			templateUrl: 'includes/createOperationalModule.html', 
 			controller: 'CreateOperationalModuleCtrl', 
-			requireLogin: true
+			requireLogin: true,
+			access: roles.user
 		}).
 		otherwise({redirectTo: '/'});
 	}])
@@ -55,6 +69,9 @@ angular.module('tracer', ['tracer.services', 'tracer.directives', 'tracer.contro
 		$rootScope.$on("$routeChangeStart", function (event, next, current) {
 			$rootScope.error = null;
 			if (!AuthService.isAuthenticated()) $location.path('/login');
+			//Restrict access to pages
+			else if (AuthService.isAuthenticated() && 
+					AuthService.getCurrentRole()<parseInt(next.access) ) $location.path('/');
 		});
 		
 	}]);
