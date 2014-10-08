35dbfc48aeb60a8ce8a9a67d7b75b1e97af94fc1
Kostas Stroggylos <stroggylos@gmail.com>
2014-10-08 23:40:12 +0300

    REST related changes before attempting to switch to Jetty/Guice/Jersey/Jackson

--- tracer/platform/src/main/java/gr/tracer/platform/controllers/SecurityProfileController.java
+++ tracer/platform/src/main/java/gr/tracer/platform/controllers/SecurityProfileController.java
@@ -7,8 +7,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import gr.tracer.common.entities.db.MonitoredProjectList;
 import gr.tracer.common.entities.db.SecurityLibrary;
@@ -17,6 +18,7 @@ import gr.tracer.common.entities.db.VulnerabilityType;
 import gr.tracer.platform.TracerPlatform;
 import gr.tracer.platform.components.SecurityProfileComponent;
 
+@Produces(MediaType.APPLICATION_JSON)
 @Path("/tracerapi")
 public class SecurityProfileController {
 	
@@ -38,14 +40,14 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#getSecurityProfiles()
      */
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile/")
 	public List<SecurityProfile> getSecurityProfiles() {
 		return spc.getSecurityProfiles();	
 	}
 	
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile2/")
 	public Response getSecurityProfiles2() {
 		return Response.ok(
@@ -56,7 +58,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#addVulnerabilityTypeToSecurityProfile(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile/addVulnerabilityType/{vtName}/{spName}")
 	public boolean addVulnerabilityTypeToSecurityProfile(@PathParam("vtName") String vtName, @PathParam("spName") String spName) {
 		return spc.addVulnerabilityTypeToSecurityProfile(vtName, spName);
@@ -66,7 +68,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#createSecurityProfile(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile/createSecurityProfile/{vtName}/{spName}")
 	public SecurityProfile createSecurityProfile(@PathParam("spName") String spName, @PathParam("spType") String spType) {
 		return spc.createSecurityProfile(spName, spType);
@@ -76,7 +78,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#searchSecurityProfile(java.lang.String)
      */
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile/get-by-name/{spName}")
 	public SecurityProfile searchSecurityProfile(@PathParam("spName") String spName) {
 		return spc.searchSecurityProfile(spName);
@@ -86,7 +88,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#removeVulnerabilityTypeFromSecurityProfile(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/SecurityProfile/removeVulnerabilityType/{vtName}/{spName}")
 	public boolean removeVulnerabilityTypeFromSecurityProfile(@PathParam("vtName") String vtName, @PathParam("spName") String spName) {
 		return spc.removeVulnerabilityTypeFromSecurityProfile(vtName, spName);
@@ -101,7 +103,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#createMonitoredProjectList(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/createMonitoredProjectList/{mplName}/{mplDescription}/{userName}/SecurityProfile/{secProfName}")
 	public MonitoredProjectList createMonitoredProjectList(@PathParam("mplName") String mplName, @PathParam("mplDescription") String mplDescription, @PathParam("userName") String userName, @PathParam("secProfName") String secProfName) {
 		return spc.createMonitoredProjectList(mplName, mplDescription, userName, secProfName);
@@ -111,7 +113,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#createMonitoredProjectList(java.lang.String, java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/createMonitoredProjectList/{mplName}/{mplDescription}/{userName}")
 	public MonitoredProjectList createMonitoredProjectList(@PathParam("mplName") String mplName, @PathParam("mplDescription") String mplDescription, @PathParam("userName") String userName) {
 		return spc.createMonitoredProjectList(mplName, mplDescription, userName);
@@ -121,7 +123,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#setSecurityProfileToList(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/setSecurityProfile/{spName}/{mplName}")
 	public boolean setSecurityProfileToList(@PathParam("spName") String spName, @PathParam("mplName") String mplName) {
 		return spc.setSecurityProfileToList(spName, mplName);
@@ -131,7 +133,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#searchMonitoredProjectList(java.lang.String)
      */
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/get-by-name/{mplName}")
 	public MonitoredProjectList searchMonitoredProjectList(@PathParam("mplName") String mplName) {
 		return spc.searchMonitoredProjectList(mplName);
@@ -141,7 +143,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#addProjectToMonitoredProjectList(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/addProject/{monProjList}/{projName}")
 	public boolean addProjectToMonitoredProjectList(@PathParam("monProjList") String monProjList, @PathParam("projName") String projName) {
 		return spc.addProjectToMonitoredProjectList(monProjList, projName);
@@ -151,7 +153,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#removeProjectFromMonitoredProjectList(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/removeProject/{monProjList}/{projName}")
 	public boolean removeProjectFromMonitoredProjectList(@PathParam("monProjList") String monProjList, @PathParam("projName") String projName) {
 		return spc.removeProjectFromMonitoredProjectList(monProjList, projName);
@@ -166,7 +168,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#getVulnerabilityTypes()
      */
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/VulnerabilityType/")
 	public List<VulnerabilityType> getVulnerabilityTypes() {
 		return spc.getVulnerabilityTypes();
@@ -181,7 +183,7 @@ public class SecurityProfileController {
 	 * @return true or false whether the method executed successful or not
 	 */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/VulnerabilityType/createVulnerabilityType/{vtName}/{vtDescription}/SecurityLibrary/{slName}/{slDescription}")
 	public boolean createVulnerabilityType(@PathParam("vtName") String vtName, @PathParam("vtDescription") String vtDescription, @PathParam("slName") String slName, @PathParam("slDescription") String slDescription) {
 		SecurityLibrary sl = spc.createSecurityLibrary(slName, slDescription);
@@ -196,7 +198,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#createVulnerabilityType(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/VulnerabilityType/createVulnerabilityType/{vtName}/{vtDescription}")
 	public VulnerabilityType createVulnerabilityType(@PathParam("vtName") String vtName, @PathParam("vtDescription") String vtDescription) {
 		return spc.createVulnerabilityType(vtName, vtDescription);
@@ -206,7 +208,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#addSecurityLibraryToVulnerabilityType(java.lang.String, java.lang.String)
      */
 	@PUT
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/addSecurityLibrary/{slName}/{vtName}")
 	public boolean addSecurityLibraryToVulnerabilityType(@PathParam("slName") String slName, @PathParam("vtName") String vtName) {
 		return spc.addSecurityLibraryToVulnerabilityType(slName, vtName);
@@ -216,7 +218,7 @@ public class SecurityProfileController {
      * @see gr.tracer.platform.components.SecurityProfileComponent#searchVulnerabilityType(java.lang.String)
      */
 	@GET
-	@Produces({"application/xml", "application/json"})
+	//@Produces({"application/xml", "application/json"})
 	@Path("/MonitoredProjectList/get-by-name/{vtName}")
 	public VulnerabilityType searchVulnerabilityType(String vtName) {
 		return spc.searchVulnerabilityType(vtName);
